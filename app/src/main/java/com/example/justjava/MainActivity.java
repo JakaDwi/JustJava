c7e43e53876ec76af75a846359614a9a2631afd6
Jaka Dwi Prasetyo <pelolong24@gmail.com>
2020-10-08 03:02:48 +0700

    data picker
    Increment & Decrement

---
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.justjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+int jumlah = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void increment(View view) {
+        jumlah = jumlah + 1;
+        display(jumlah);
+    }
+
+    public void decrement(View view) {
+        if (jumlah > 0 == false) {
+        } else {
+            jumlah = jumlah - 1;
+            display(jumlah);
+        }
+    }
+    public void submitOrder(View view) {
+        displayHarga(jumlah * 1000);
+    }
+
+    private void display(int number) {
+        TextView jumlahTextView = (TextView) findViewById(R.id.jumlah_text_view);
+        jumlahTextView.setText("" + number + " Biji");
+    }
+
+    private void displayHarga(int number){
+        Locale localeID = new Locale("in", "ID");
+        NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
+            TextView hargaTextView = (TextView) findViewById(R.id.harga_text_view);
+            hargaTextView.setText(formatRupiah.format(number));
+    }
+}
